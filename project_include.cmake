57a1ffb7773b5ab2d9353e4b9c5c6374d9950ca3
Пионтик Роман <rip@rabota.ru>
2019-10-13 10:22:17 +0300

    Remove project_include.cmake

--- project_include.cmake
+++
@@ -1,3 +0,0 @@
-message(">>>>>>>>>>>>>>>> !!!!!!!!!!!!!!!!!!! <<<<<<<<<<<<<<<<<<")
-add_library(thingjs_core STATIC IMPORTED empty.c)
-set_property(TARGET thingjs_core PROPERTY IMPORTED_LOCATION /home/rpiontik/opensources/ThingJS.IO/ThingJS-template/components/thingjs_core/libthingjs_core.a)
