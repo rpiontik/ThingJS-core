1986afc9079ae94aa0401def1ece1a52a42b59c3
Пионтик Роман <rip@rabota.ru>
2019-10-21 16:08:23 +0300

    Update refactored library for extract interface to stdi component

--- thingjs_core.h
+++ thingjs_core.h
@@ -8,6 +8,22 @@
 #ifndef MAIN_CORE_CORE_H_
 #define MAIN_CORE_CORE_H_
 
+#include "mjs.h"
+#include "cJSON.h"
+
+//Pin combinations cases
+typedef int * thingjs_pin_cases;
+
+//Constructor of ThingsJS interface
+typedef mjs_val_t (*thingjs_interface_constructor)(struct mjs * mjs, cJSON * params);
+
+//Interface manifest
+struct st_thingjs_interface_manifest {
+    char *	type;							    //Name of interface type
+    thingjs_interface_constructor constructor;	//JS interface
+    thingjs_pin_cases cases; 				    //Configurations cases
+};
+
 //Initialization and start all core services
 void thingjsInit(void);
 
@@ -16,5 +32,8 @@ void thingjsStart(void);
 
 //Main runtime function
 void thingjsHandle(void);
+
+//Register ThingJS interface
+void thingjsRegisterInterface(const struct st_thingjs_interface_manifest * interface);
 
 #endif /* MAIN_CORE_CORE_H_ */
